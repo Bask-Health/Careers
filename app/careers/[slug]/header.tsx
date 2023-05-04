77696bcb7ff108daf32cf373ba08d60545bbc684
inveve <zachary@inveve.com>
2023-05-03 22:26:55 -0400

    Fixes

--- app/careers/[slug]/header.tsx
+++ app/careers/[slug]/header.tsx
@@ -67,7 +67,7 @@ export const Header: React.FC<Props> = ({ project, views }) => {
 								views,
 							)}
 						</span>
-						<Link target="_blank" href="https://twitter/baskhealth">
+						<Link target="_blank" href="https://twitter.com/baskhealth">
 							<Twitter
 								className={`w-6 h-6 duration-200 hover:font-medium ${
 									isIntersecting
@@ -76,7 +76,7 @@ export const Header: React.FC<Props> = ({ project, views }) => {
 								} `}
 							/>
 						</Link>
-						<Link target="_blank" href="https://github.com/baskhealth">
+						<Link target="_blank" href="https://github.com/bask-health">
 							<Github
 								className={`w-6 h-6 duration-200 hover:font-medium ${
 									isIntersecting
